bbbdb83e8c883d4f4b8b60c49d0a125c8ca4a809
kirill <brigadirkirill@yandex.ru>
2025-07-17 16:23:43 +0300

    третье задание готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+   return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
+
+
+
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let newArray = users.filter (element => element.gender === gender);
+    let newArrayGender = newArray.map (element => element.age);
+    let result = newArrayGender.reduce((acc, item, index)=> {
+        acc+=item;
+        if(index === newArrayGender.length - 1){
+            return acc / newArrayGender.length;
+        }
+            return acc;
+    }, 0);
+    return result;
 }
+  
