8c2e9107d50f3b00c3b34284299bc3f4a459f792
kirill <brigadirkirill@yandex.ru>
2025-07-17 17:44:53 +0300

    четвертое задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(this.hasOwnProperty('marks')) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.hasOwnProperty('marks') && this.marks.length !== 0) {
+        let average = this.marks.reduce((accumulator, items) => {
+            return accumulator + items / this.marks.length;
+        }, 0)
+        return average;
+        } else {
+        return 0;   
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
 }
